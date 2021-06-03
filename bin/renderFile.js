e05fa3f30fd4ca1419359b9547be0f97a018773f
Yvan2415 <33819349+Yvan2415@users.noreply.github.com>
2021-06-03 10:03:11 +0800

    对main.js文件进行渲染

--- bin/renderFile.js
+++ bin/renderFile.js
@@ -6,10 +6,16 @@ module.exports = function(options, name){
   // 这是创建的项目的路径
   const dir = path.join(process.cwd(), name);
   
-  // 修改package.json模板
+  // 渲染package.json模板
   ejs.renderFile(path.join(dir, 'package.json'), options, function(err, result){
     if(err) throw err;
     fs.writeFileSync(path.join(dir, 'package.json'), result);
+  })
+  
+  // 渲染main.js
+  ejs.renderFile(path.join(dir, 'src/main.js'), options, function(err, result){
+    if(err) throw err;
+    fs.writeFileSync(path.join(dir, 'src/main.js'), result);
   })
 
 }
